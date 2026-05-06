93afdec20887b5cc15a39afd66acacba45adf909
RaisyaPuspa23 <email_raisyapuspa23@gmail.com>
2026-05-07 00:13:39 +0700

    feat: perbaikan navigasi dan halaman produk

--- lib/core/router/app_router.dart
+++ lib/core/router/app_router.dart
@@ -1,12 +1,19 @@
 import 'package:go_router/go_router.dart';
 import '../../features/product/presentation/pages/splash/splash_page.dart';
+import '../../features/product/presentation/pages/product_page.dart'; 
 
 final router = GoRouter(
   initialLocation: '/',
+  // Ini untuk menangani kalau ada rute yang nyasar agar tidak muncul layar putih "Page Not Found"
+  errorBuilder: (context, state) => const ProductPage(), 
   routes: [
     GoRoute(
       path: '/',
       builder: (context, state) => const SplashPage(),
+    ),
+    GoRoute(
+      path: '/home',
+      builder: (context, state) => const ProductPage(), 
     ),
   ],
 );
