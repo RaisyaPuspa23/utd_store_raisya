40233661d5598eeedb698539116478af3cd1ecad
RaisyaPuspa23 <email_raisyapuspa23@gmail.com>
2026-05-06 07:51:35 +0700

    Setup clean architecture and product features

---
+++ lib/features/product/data/repositories/product_repository_impl.dart
@@ -0,0 +1,33 @@
+import 'package:dio/dio.dart';
+import '../../domain/entities/product.dart';
+import '../../data/repositories/product_repository_impl.dart';
+// import domain entity...
+
+class ProductRepositoryImpl {
+  final Dio dio;
+  ProductRepositoryImpl(this.dio);
+
+  Future<List<Product>> getProducts() async {
+    try {
+      final response = await dio.get('https://fakestoreapi.com/products');
+      final List data = response.data;
+
+      return data.map((item) {
+        // LOGIKA NIM GANJIL: Tambahkan [Diskon 10%] di layer Service/Repo
+        // Sesuai NIM Raisya 20123007
+        String originalTitle = item['title'];
+        String modifiedTitle = "$originalTitle [Diskon 10%]"; 
+
+        return Product(
+          id: item['id'],
+          title: modifiedTitle,
+          price: (item['price'] as num).toDouble(),
+          description: item['description'],
+          image: item['image'],
+        );
+      }).toList();
+    } catch (e) {
+      throw Exception("Gagal ambil data");
+    }
+  }
+}
